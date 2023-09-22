164bd76dfe34dc7c5fb115ec9581c3292f45e074
janwodniak <janwodniak@gmail.com>
2023-09-22 20:49:56 +0200

    Initial commit

---
+++ src/main/java/dev/janwodniak/reactivecrud/ReactiveCrudApplication.java
@@ -0,0 +1,13 @@
+package dev.janwodniak.reactivecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/janwodniak/reactivecrud/ReactiveCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.janwodniak.reactivecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
